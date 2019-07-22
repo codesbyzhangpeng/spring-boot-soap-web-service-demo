d54e90ed02864e18cfbadc75cbec382e8a04b627
czp <czp_182000@hotmail.com>
2019-07-22 14:19:10 +0200

    Spring Boot -> SOAP Web Service: Course Management

---
+++ soap-course-managment/src/main/java/com/springboot/soap/webservice/SoapCourseManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.soap.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagmentApplication.class, args);
+	}
+
+}
